58ea5a75f44882c0fc4325e430ba21ffe8922145
rikusasaki4008 <lewisalice4008@gmail.com>
2023-05-13 12:52:52 +0900

    Springのファイルを追加

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(name, email, create_at, update_at)
+VALUES('鈴木一郎', 'suzuki@example.com', '2023-11-12 08:34:19', '2023-11-12 08:34:19');
+INSERT INTO users(name, email, create_at, update_at)
+VALUES('佐藤二郎', 'satou@example.com', '2023-12-18 12:10:52', '2023-12-18 12:10:52');
+INSERT INTO users(name, email, create_at, update_at)
+VALUES('田中三郎', 'tanaka@example.com', '2023-12-18 15:10:52', '2023-12-18 15:10:52');
